f7abc2d6169227115ad857691e5c0b366c1e3827
niaidaye <645992178@qq.com>
2017-11-05 22:18:39 +0800

    完成Calendar的测试

--- Java程序设计概述/src/CalendarTest.java
+++ Java程序设计概述/src/CalendarTest.java
@@ -23,5 +23,58 @@ public class CalendarTest {
 
         int firstDayOfWeek = d.getFirstDayOfWeek();
 
+        //determine the required indentation for the first line
+        int indent = 0;
+        while (weekday != firstDayOfWeek)
+        {
+            indent++;
+            d.add(Calendar.DAY_OF_MONTH, -1);
+            weekday = d.get(Calendar.DAY_OF_WEEK);
+        }
+
+        //print weekday names
+        String[] weekdayNames = new DateFormatSymbols().getShortWeekdays();
+        do {
+            System.out.printf("%4s", weekdayNames[weekday]);
+            d.add(Calendar.DAY_OF_MONTH, -1);
+            weekday = d.get(Calendar.DAY_OF_WEEK);
+        }
+        while (weekday != firstDayOfWeek);
+        System.out.println();
+        for (int i = 1; i <= indent; i++){
+            System.out.print(" ");
+        }
+
+        d.set(Calendar.DAY_OF_MONTH, 1);
+        do
+        {
+            // print day
+            int day = d.get(Calendar.DAY_OF_MONTH);
+            System.out.printf("%3d", day);
+
+            //mark current day with *
+            if (day == today) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+
+            //advance d to the next day
+            d.add(Calendar.DAY_OF_MONTH, 1);
+            weekday = d.get(Calendar.DAY_OF_WEEK);
+
+            //start a new line at the start of the start of the week
+            if (weekday == firstDayOfWeek) {
+                System.out.println();
+            }
+        }
+
+            while (d.get(Calendar.MONTH) == month);
+        {
+
+            if (weekday != firstDayOfWeek){
+                System.out.println();
+            }
+        }
     }
 }
